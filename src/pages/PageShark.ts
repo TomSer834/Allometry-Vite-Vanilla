d816dddedcd0849119bc9d6ef9e5cbcdcf1fe689
TomSer834 <tom.mailing@gmail.com>
2023-07-27 01:37:03 +0200

    changed images links to root

--- src/pages/PageShark.ts
+++ src/pages/PageShark.ts
@@ -14,9 +14,9 @@ export const PageShark = () => {
 
     <div class="sharks">
         <div></div>
-        <div class="sh-left"><img src="/public/shark-left-3c.png"></div>
+        <div class="sh-left"><img src="./shark-left-3c.png"></div>
         <div></div>
-        <div class="sh-right"><img src="/public/shark-right.png"></div>
+        <div class="sh-right"><img src="./shark-right.png"></div>
     </div>
 
     <div class="main-substitute">
